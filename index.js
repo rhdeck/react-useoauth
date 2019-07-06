79702e0c528169f825696a448a391c57e6694cd3
Ray Deck <ray@raydeck.com>
2019-07-05 20:40:24 -0400

    Reverting to old args spread

--- index.js
+++ index.js
@@ -9,22 +9,13 @@ const useOauth = ({
   state,
   args
 }) => {
-  if (!args) args = {};
-  const base = {
+  const url = buildURL(authorizeUrl, {
     client_id: clientId,
     redirect_uri: redirectUri,
     response_type: "code",
-    state: state ? JSON.stringify(state) : undefined
-  };
-  const url = buildURL(
-    authorizeUrl,
-    args
-      ? Object.entries(args).reduce((o, [k, v]) => {
-          o[k] = v;
-          return o;
-        }, base)
-      : base
-  );
+    state: state ? JSON.stringify(state) : undefined,
+    ...(args || {})
+  });
   const [code, setCode] = useState();
   const [status, setStatus] = useState();
   const [returnedState, setReturnedState] = useState();
